43c6363bbeafa4fcb5e7c2c61b77dc0a23250b9d
davidvalenciaz <mkcallese@gmail.com>
2024-01-20 20:55:54 -0500

    se importan y agregan los componentes a la app

--- src/App.js
+++ src/App.js
@@ -1,2 +1,21 @@
 
 
+import './App.css';
+import About from './components/About';
+import Footer from './components/Footer';
+import Info from './components/Info'
+import Interest from './components/Interest';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Info/>
+      <About/>
+      <Interest/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
