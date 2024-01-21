4f5347fedd218a7da1a4db72b7b1bd5d2fbe8024
davidvalenciaz <mkcallese@gmail.com>
2024-01-20 21:12:52 -0500

    agregando typeanimation a react

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import logo_profile from "../images/profile-logo.png";
+import { TypeAnimation } from "react-type-animation";
 
 function info() {
   return (
     <div className="info">
       <img src={logo_profile} alt="" />
       <h1>Juan David Valencia </h1>
+      <div>
+        <TypeAnimation
+          sequence={["creativo", "recursivo", "innovador"]}
+          typeSpeed={50}
+          backSpeed={50}
+          loop
+        />
+      </div>
       <button className="btn-contact">Contact Me</button>
       <button className="btn-linke">Contact Me</button>
     </div>
