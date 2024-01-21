1b4dbaf97940f1820eaa6313d19f506a494a93f1
davidvalenciaz <mkcallese@gmail.com>
2024-01-20 20:48:29 -0500

    se crea componente interest

---
+++ src/components/Interest.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Interest() {
+  return (
+    <div className="interest">
+      <p>
+        Estoy interesando en la industria del el desarrollo web , expandir mis
+        conocimientos como full stack , en las diferentes tecnologias que este
+        me pueda brindar , apuntando siempre a la excelencia dentro de mi campo
+        laboral,creciendo asi como persona y profesional 
+      </p>
+    </div>
+  );
+}
+
+export default Interest;
