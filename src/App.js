066cf357c83fb8b836ebc75c0e7ee510b13bc428
davidvalenciaz <mkcallese@gmail.com>
2024-01-20 23:24:45 -0500

    Creando estilos Css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
+import "./App.css";
 
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Info from "./components/Info";
+import Interest from "./components/Interest";
 
-import './App.css';
-import About from './components/About';
-import Footer from './components/Footer';
-import Info from './components/Info'
-import Interest from './components/Interest';
 
 
 function App() {
   return (
     <div className="App">
-      <Info/>
-      <About/>
-      <Interest/>
-      <Footer/>
+      <Info />
+      <About />
+      <Interest />
+      <Footer />
+      
     </div>
   );
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import logo_profile from "../images/profile-logo.png";
 import { TypeAnimation } from "react-type-animation";
+import like_logo from '../images/linkedin.png'
 
 function info() {
   return (
     <div className="info">
       <img src={logo_profile} alt="" />
       <h1>Juan David Valencia </h1>
-      <div>
+      <div className="txt-animation">
         <TypeAnimation
-          sequence={["creativo", "recursivo", "innovador"]}
-          typeSpeed={50}
-          backSpeed={50}
+          sequence={["Creativo ", "Recursivo ", "Innovador "]}
+          typeSpeed={150}
+          backSpeed={150}
           loop
         />
       </div>
       <button className="btn-contact">Contact Me</button>
-      <button className="btn-linke">Contact Me</button>
+      <button className="btn-linke">linkedin</button>
     </div>
   );
 }
