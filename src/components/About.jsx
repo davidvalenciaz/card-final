0d71228e25de9724eda4b9d4b299550c9c760b5a
davidvalenciaz <mkcallese@gmail.com>
2024-01-20 20:46:21 -0500

    se crea componente about

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <p>
+        Desarrollador full stack con conocimientos en Javascript, Java , Bases
+        de datos , Arquitectura de datos , actualmente me encuentro estudiando
+        en la escual Codigo C13
+      </p>
+    </div>
+  );
+}
+
+export default About;
